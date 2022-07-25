b93ce01eb41fe62d7c83d3e7a9d6deaff93ee304
Alfi Setyadi <alfisetyadi@gmail.com>
2022-07-25 17:28:12 +0700

    typeorm setup and users table

---
+++ src/typeorm/User.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    nullable: false,
+  })
+  username: string;
+
+  @Column({
+    nullable: false,
+  })
+  email: string;
+
+  @Column({
+    nullable: false,
+  })
+  password: string;
+}
